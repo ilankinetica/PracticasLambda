7b9217aca8bff41af4e8ed7c668f6c669f8af44d
BrenBorys <brenda@kinetica-solutions.com>
2016-04-12 16:27:20 -0300

    class documento, firmante funciones firmantemayor, generarfirmantealeatorio

--- PracticasLambda/Program.cs
+++ PracticasLambda/Program.cs
@@ -8,8 +8,44 @@ namespace PracticasLambda
 {
     class Program
     {
+        class Documento
+        {
+            public string Titulo { get; set; }
+            public string Cuerpo { get; set; }
+            public List<Firmante> Firmantes { get; set; }
+        }
+        class Firmante
+        {
+            public string Nombre { get; set; }
+            public string Firma { get; set; }
+            public int Edad { get; set; }
+        }
         static void Main(string[] args)
         {
+        }
+        static bool FirmanteMayor (Documento documento)
+        {
+            bool contieneMayor = false;
+            foreach (var persona in documento.Firmantes)
+            {
+                if (true)
+                {
+                    contieneMayor = true;
+                }
+            }
+            return contieneMayor; 
+        }
+        Documento GenerarDocumentoAleatorioCon5Firmantes()
+        {
+        }
+        Firmante GenerarFirmanteAleatorio()
+        {
+            var firmante = new Firmante();
+            var aleatorio = new Random();
+            firmante.Nombre = aleatorio.Next(0, 100).ToString();
+            firmante.Firma = aleatorio.Next(0, 100).ToString();
+            firmante.Edad = aleatorio.Next(10, 70);
+            return firmante;
         }
     }
 }
