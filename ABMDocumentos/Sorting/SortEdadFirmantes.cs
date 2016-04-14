e493728d22c8dd412bf1e38b7c7e4aa3255b7d1d
ilanolkies <ilan@kinetica-solutions.com>
2016-04-14 11:32:41 -0300

    sort edad firmantes

---
+++ ABMDocumentos/Sorting/SortEdadFirmantes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMDocumentos.Sorting
+{
+    class SortEdadFirmantes : IComparer<Firmante>
+    {
+        /// <summary>
+        /// Compara dos objetos y devuelve un valor que determina el orden alfabetico
+        /// </summary>
+        /// <param name="x">Primer objeto a comparar</param>
+        /// <param name="y">Segundo objeto a comparar</param>
+        /// <returns>0 si son iguales, -1 si y es mayor, 1 si x es mayor</returns>
+        public int Compare(Firmante x, Firmante y)
+        {
+            if (x.Edad == y.Edad)
+            {
+                return 0;
+            }
+            else if (x.Edad > y.Edad)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
