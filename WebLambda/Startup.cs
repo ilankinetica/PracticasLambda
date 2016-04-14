13447031310b45499fc06c850362ff0e3fce8998
ilanolkies <ilan@kinetica-solutions.com>
2016-04-14 11:28:40 -0300

    libreria de clases y mvc

---
+++ WebLambda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLambda.Startup))]
+namespace WebLambda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
