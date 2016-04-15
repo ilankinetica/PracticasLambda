972844d17b1a0d1f62facf70c42cf0a20cbf2d76
ilanolkies <ilan@kinetica-solutions.com>
2016-04-15 10:27:57 -0300

    FormatoFunciones.cs varios documentos

---
+++ ABMDocumentos/FormatoFunciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMDocumentos
+{
+    public class FormatoFunciones
+    {
+        string DocumentosVarios (List<Documento> documentos, string encabezado)
+        {
+            string formateado = "";
+            try
+            {
+                formateado += encabezado + "\n\n";
+                foreach (var documento in documentos)
+                {
+                    formateado += "\n\nDocumento: \nTitulo: " + documento.Titulo + "\nCuerpo: " + documento.Cuerpo + "\nFirmantes: \n";
+                    foreach (var firmante in documento.Firmantes)
+                    {
+                        formateado += "\nNombre: " + firmante.Nombre + "\nFirma: " + firmante.Firma + "\nEdad: " + firmante.Edad.ToString() + "\n-----";
+                    }
+                    formateado += "\n----- -----";
+                }
+                formateado += "\n----- ----- -----\n";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return formateado;
+        }
+    }
+}
